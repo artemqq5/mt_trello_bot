8cb9ac380d639ae7db3f3ced391c33a42f6bdcfe
KatePonomarenko <52855607+artemqq5@users.noreply.github.com>
2023-03-04 14:36:57 +0200

    final dev gambling PPC
    add (set_domain, setting_cloak, prepare_vait) to gambling PPC

--- bot_helper/gambling_ppc_buttons.py
+++ bot_helper/gambling_ppc_buttons.py
@@ -6,9 +6,9 @@ def gambling_ppc_menu():
     markup = types.InlineKeyboardMarkup()
 
     listButtons = (
-        types.InlineKeyboardButton('Подготовить вайт', callback_data="prepare_vait"),  # one
-        types.InlineKeyboardButton('Настроить клоаку', callback_data="setting_cloak"),  # one
-        types.InlineKeyboardButton('Припарковать домен', callback_data="set_domain"),  # one
+        types.InlineKeyboardButton('Подготовить вайт 🎁', callback_data="prepare_vait"),  # one
+        types.InlineKeyboardButton('Настроить клоаку 📡', callback_data="setting_cloak"),  # one
+        types.InlineKeyboardButton('Припарковать домен 🅿️', callback_data="set_domain"),  # one
         types.InlineKeyboardButton('Создать кампанию 🔖', callback_data="create_campaign"),
         types.InlineKeyboardButton('Заказать Креатив 🪄', callback_data="order_creative"),
         types.InlineKeyboardButton('Другое задание 💻', callback_data="other_task"),
