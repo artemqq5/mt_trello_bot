5646584e9b65146fdc83db2d5ed2d008e0149b6d
artem <artemqq5@gmail.com>
2024-07-13 00:10:13 +0300

    cmt

--- handlers/tech/tools/notify_tech.py
+++ handlers/tech/tools/notify_tech.py
@@ -1,5 +1,6 @@
+from config.private_config import GLEB_ID, EGOR_ID
 from constants.creo import DESIGN
-from constants.dep import DESIGNER_, TECH_
+from constants.dep import DESIGNER_, TECH_, ADMIN_
 from repository.card_repository.card_repos import CardRepository
 from repository.user_repository.user_repos import UserRepository
 from repository.user_repository.user_usecase import get_user_model_list
@@ -14,9 +15,9 @@ async def notify_new_tech(message, trellocard, url, tech):
 
     try:
         for user in users:
-            if user.dep == TECH_:
-                # if tech == "gleb":
-                await message.bot.send_message(chat_id=user.id, text=info_task)
+            if user.dep == TECH_ or user.dep == ADMIN_:
+                if tech == "gleb" and user.id == GLEB_ID or tech == "egor" and user.id == EGOR_ID or user.dep == ADMIN_:
+                    await message.bot.send_message(chat_id=user.id, text=info_task)
     except Exception as e:
         print(f"notify_new_tech: {e}")
 
